98542dcbf92c4f38702dfac3ff48a706c56fcb71
bonnie the bunny13 <gibi.game13@gmail.com>
2019-10-01 19:31:51 -0300

    Adicionado os Arquivos discord-kali-linux.sh e discord.deb

---
+++ discord-kali-linux.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+clear
+echo "Criador:bonnie the bunny13";
+echo "Discord:https://discord.gg/V2sjVHN";
+
+clear
+echo -n "Loading" 
+for i in $(seq 1 1 8); 
+do # Faça
+	echo -n "." 
+	sleep 001 
+	echo -ne "." 
+done
+clear
+
+chmod +x discord.deb
+echo "Discord Instalando";
+dpkg -i discord.deb
+clear
+
+echo "Discord Instalado";
+echo "Criador:bonnie the bunny13";
+echo "Discord:https://discord.gg/V2sjVHN";
+
+clear
+echo -n "Loading" 
+for i in $(seq 1 1 8); 
+do # Faça
+	echo -n "." 
+	sleep 001 
+	echo -ne "." 
+done
+clear
+
+echo "Criador:bonnie the bunny13";
+echo " " 
+echo "Discord:https://discord.gg/V2sjVHN";
+echo " "
+echo "Para Iniciar o Discord Basta Digitar discord No Terminal Do Kali Linux";
